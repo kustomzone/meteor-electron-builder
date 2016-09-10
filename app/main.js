ea1289150a0e0cd63ce16613061378fcd38ad3bf
menelike <menelike@users.noreply.github.com>
2016-09-10 21:15:35 +0200

    refactor windows squirrel startup handler

--- app/main.js
+++ app/main.js
@@ -1,69 +1,16 @@
 /* eslint-disable no-console */
 const app = require('electron').app; // Module to control application life.
-const spawn = require('child_process').spawn;
 const path = require('path');
 const fs = require('fs');
 const BrowserWindow = require('electron').BrowserWindow; // Module to create native browser window.
-const autoUpdater = require('./autoUpdater');
+const AppUpdater = require('./AppUpdater');
+const handleStartupEvent = require('./winSquirrelStartupEventHandler');
 const createDefaultMenu = require('./menu.js');
 const proxyWindowEvents = require('./proxyWindowEvents');
 const _ = require('lodash');
 require('electron-debug')({ showDevTools: false });
 
 const settings = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8'));
-
-function run(args, done) {
-  const updateDotExe = path.resolve(path.dirname(process.execPath), '..', 'update.exe');
-  const child = spawn(updateDotExe, args, { detached: true });
-  child.on('close', done);
-}
-
-const handleStartupEvent = () => {
-  var result = false;
-
-  if (process.platform !== 'win32') return false;
-
-  const squirrelCommand = process.argv[1];
-  const target = path.basename(process.execPath);
-  switch (squirrelCommand) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-      console.info('SQUIRREL INSTALL/UPDATE');
-      // Optionally do things such as:
-      //
-      // - Install desktop and start menu shortcuts
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
-
-      // Always quit when done
-      run(['--createShortcut', target], app.quit);
-      result = true;
-      break;
-    case '--squirrel-uninstall':
-      console.info('SQUIRREL UNINSTALL');
-
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Always quit when done
-      run(['--removeShortcut', target], app.quit);
-      app.quit();
-      result = true;
-      break;
-    case '--squirrel-obsolete':
-      console.info('SQUIRREL OBSOLETE');
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-      app.quit();
-      result = true;
-      break;
-    default:
-      result = false;
-  }
-  return result;
-};
 
 if (handleStartupEvent()) return;
 
@@ -92,7 +39,7 @@ var mainWindow = null;
 
 // Unfortunately, we must set the menu before the application becomes ready and so before the main
 // window is available to be passed directly to `createDefaultMenu`.
-createDefaultMenu(app, () => mainWindow, () => autoUpdater.checkForUpdates(true));
+createDefaultMenu(app, () => mainWindow, () => AppUpdater.checkForUpdates(true));
 
 const hideInsteadofClose = e => {
   mainWindow.hide();
